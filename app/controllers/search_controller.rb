ab60986285d1fdcd15959c5bc2fa75eaba98e6a7
Jaap Wijnen <jaapwijnen@hotmail.com>
2014-02-22 01:47:13 +0100

    small search fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,12 @@ class SearchController < ApplicationController
 		@search_sgid = 			Course.basic_search(sgid: params[:q])
 		@search_staff = 		Staff.basic_search(name: params[:q])
 
-		search_title = 			Course.basic_search(name: params[:q])
-		search_description = 	Course.basic_search(description: params[:q])
-		
-		@search_general = 		search_title + search_description
-		@search_general.uniq!
+		if @search_sgid.empty? && @search_staff.empty?
+			search_title = 			Course.basic_search(name: params[:q])
+			search_description = 	Course.basic_search(description: params[:q])
+			
+			@search_general = 		search_title + search_description
+			@search_general.uniq!
+		end
 	end
 end
